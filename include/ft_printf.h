ff425c3e49936eac5c0ce25de28db89627beee29
David Jimenez Perafan <dajimene@c3r8s5.42urduliz.com>
2023-01-20 13:38:28 +0100

    finished

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dajimene <dajimene@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 14:00:01 by dajimene          #+#    #+#             */
-/*   Updated: 2023/01/10 15:04:36 by dajimene         ###   ########.fr       */
+/*   Updated: 2023/01/02 20:33:56 by dajimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <stdint.h>
 # include <unistd.h>
 
-int	ft_printf(const char *s, ...);
+int	ft_printf(char const *format, ...);
 int	ft_putchar(int c);
 int ft_printstr(char *str);
 int	ft_printnbr(int n);
 int	ft_printpercent(void);
 int	ft_print_unsigned(unsigned int n);
 int	ft_print_ptr(unsigned long long ptr);
-int	ft_print_hex(unsigned long long num, const char format);
+int	ft_print_hex(unsigned long long s, const char format);
 #endif
--- src/ft_print_unsigned.c
+++ src/ft_print_unsigned.c
@@ -6,17 +6,41 @@
 /*   By: dajimene <dajimene@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 12:29:31 by dajimene          #+#    #+#             */
-/*   Updated: 2023/01/17 11:29:15 by dajimene         ###   ########.fr       */
+/*   Updated: 2023/01/20 13:24:50 by dajimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 #include "../include/libft.h"
 
+char*ft_utoa(unsigned int  number)
+{
+	char	*num;
+	int		len;
+	unsigned int	n;
+
+	len = 0;
+	n = number;
+	while (n != 0)	{
+		len++;
+		n /= 10;
+	}
+	num = (char *)malloc(sizeof(char)* (len + 1));
+	if(!num)
+		return(0);
+	num[len] = '\0';
+	while (len--)
+	{
+		num[len] = number % 10 + 48;
+		number /= 10;
+	}
+	return(num);
+}
+
 int	ft_print_unsigned(unsigned int n)
 {
 	char	*num;
-	char	*aux;
+	//char	*aux;
 	int		len;
 	
 	len = 0;
@@ -24,11 +48,11 @@ int	ft_print_unsigned(unsigned int n)
 		len += write(1, "0", 1);
 	else
 	{
-		num = ft_itoa(n);
-		aux = num;
-		aux = ft_strtrim(num, "-");
-		len += ft_printstr(aux);
-		free(aux);
+		num = ft_utoa(n);
+		//aux = num;
+		//num = ft_strtrim(num, "-");
+		//free(aux);
+		len += ft_printstr(num);
 		free(num);
 	}
 	return (len);
